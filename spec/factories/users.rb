8ba47af26b7e8dea0432db2516d0134082b62702
Milt Reder <milt@jhu.edu>
2013-02-22 13:01:26 -0500

    spec test for things controller, some tweaks for devise and rolify

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,5 +7,10 @@ FactoryGirl.define do
     f.password_confirmation 'changeme'
     # required if the Devise Confirmable module is used
     # confirmed_at Time.now
+
+  
+    factory :admin do
+      after(:create) {|user| user.add_role :admin }
+    end
   end
 end
