d218f0a22c39f1a45033f807cd1eee8c13272435
Milton Reder <milt@jhu.edu>
2013-02-25 13:22:14 -0500

    fixed with callback. also required db cleaner, though this shouldnt be needed

--- spec/factories/checkouts.rb
+++ spec/factories/checkouts.rb
@@ -5,11 +5,11 @@ FactoryGirl.define do
     user
     pickup_at DateTime.now
     return_at DateTime.now + 1.day
-    allocations {
+    after(:create) do |checkout|
       Array(1..10).sample.times.map do
-        FactoryGirl.build(:allocation)
+        FactoryGirl.create(:allocation, checkout: checkout)
       end
-    }
-    after(:create) {|checkout| checkout.allocations.each {|a| a.save}}
+    end
   end
 end
+
