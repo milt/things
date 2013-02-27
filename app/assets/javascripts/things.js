d4531ae6e1b53fc63e1bade6406bc909e471728e
Milton Reder <milt@jhu.edu>
2013-02-27 16:23:28 -0500

    trying to make typeahead work

--- app/assets/javascripts/things.js
+++ app/assets/javascripts/things.js
@@ -2,3 +2,7 @@ $("#things-search").submit(function() {
     $.get($(this).attr("action"), $(this).serialize(), null, "script");
     return false;
   });
+
+$(function() {
+  $("#q_name_cont").typeahead()
+}
