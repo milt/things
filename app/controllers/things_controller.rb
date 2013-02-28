5eaa86cb59baa82773fbb51a1d4b36a86eb6b439
Milt Reder <milt@jhu.edu>
2013-02-28 11:31:31 -0500

    kam views

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -6,7 +6,7 @@ class ThingsController < ApplicationController
   # GET /things.json
   def index
     @q = Thing.search(params[:q])
-    @things = @q.result(:distinct => true)
+    @things = @q.result(:distinct => true).page params[:page]
     @typeahead_names = Thing.pluck(:name)
 
     respond_to do |format|
